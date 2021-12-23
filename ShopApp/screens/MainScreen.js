968bc8770404142a86c7ec90d4fc4bc82e1b8119
Ngô Quốc Vinh <71309432+ngongo12@users.noreply.github.com>
2021-12-23 18:51:04 +0700

    config for notification success

--- ShopApp/screens/MainScreen.js
+++ ShopApp/screens/MainScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import messaging from '@react-native-firebase/messaging';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import userActions from '../actions/userActions';
@@ -13,21 +13,55 @@ import NotificationScreen from './NotificationScreen';
 import StoreScreen from './Stores/StoreScreen';
 import ProfileScreen from './Profiles/ProfileScreen';
 import { getData, clearAllData } from '../api/asyncStorage';
+//import { messaging } from '../config/firebase';
 const Tab = createBottomTabNavigator();
 
 const MainScreen = (props) => {
-    const { user: { user }, cActions, cart } = props;
+    const { user: { user }, cActions, cart, actions } = props;
+    const [newToken, setNewToken] = useState();
     useEffect(() => {
         //clearAllData();
-        
+
         getData(user._id)
-        .then(res => cActions.load(res))
-        .catch(e => console.log(e));
+            .then(res => cActions.load(res))
+            .catch(e => console.log(e));
+
+        getDeviceToken();
+
     }, [user])
+
+    useEffect(() => {
+        //console.log('newToken', !newToken)
+        if (newToken && newToken !== user.token) {
+            //console.log('update token')
+            actions.actionEditProfile({
+                user: {
+                    _id: user._id,
+                    token: newToken
+                }
+            })
+        }
+    }, [newToken])
+    try {
+        if (!messaging()?.isDeviceRegisteredForRemoteMessages) {
+            messaging().registerDeviceForRemoteMessages();
+        }
+    }catch(e){
+        console.error(e);
+    }
+    //console.log('isDeviceRegisteredForRemoteMessages: ', messaging().isDeviceRegisteredForRemoteMessages);
+    const getDeviceToken = () => {
+        messaging().getToken().then(token => setNewToken(token))
+            .catch(e => console.error(e))
+    }
+    useEffect(() => {
+        messaging().hasPermission().then(res => console.log('hasPermission', res))
+    }, [])
+
     //console.log(cart)
-    return(
+    return (
         <Tab.Navigator
-            screenOptions={({route}) => ({
+            screenOptions={({ route }) => ({
                 headerShown: false,
                 tabBarStyle: {
                     backgroundColor: MainColor,
@@ -38,18 +72,18 @@ const MainScreen = (props) => {
                 tabBarInactiveTintColor: 'silver',
                 tabBarHideOnKeyboard: true,
                 tabBarShowLabel: false,
-                tabBarIcon: ({ color, size}) => {
+                tabBarIcon: ({ color, size }) => {
                     const icons = {
                         HomeScreen: 'home',
                         NotificationScreen: 'bells',
                         StoreScreen: 'isv',
                         ProfileScreen: 'user'
                     };
-                    return(
+                    return (
                         <Icon
-                            name = {icons[route.name]}
-                            color = {color}
-                            size = {size}
+                            name={icons[route.name]}
+                            color={color}
+                            size={size}
                         />
                     )
                 }
@@ -57,16 +91,16 @@ const MainScreen = (props) => {
         >
             <Tab.Screen
                 name='HomeScreen' component={HomeScreen}
-                
+
             />
             <Tab.Screen
                 name='StoreScreen' component={StoreScreen}
-                
+
             />
             <Tab.Screen
                 name='NotificationScreen' component={NotificationScreen}
             />
-            
+
             <Tab.Screen
                 name='ProfileScreen' component={ProfileScreen}
             />
