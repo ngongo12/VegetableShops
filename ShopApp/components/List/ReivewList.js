3c0e17af7ed079c7df6c1bcdfd329af90e192441
Ngô Quốc Vinh <71309432+ngongo12@users.noreply.github.com>
2022-01-24 14:02:30 +0700

    24/1

--- ShopApp/components/List/ReivewList.js
+++ ShopApp/components/List/ReivewList.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         borderBottomWidth: 0.5,
         borderColor: 'silver',
-        paddingTop: 5
+        paddingTop: 10
     },
     image: {
         width: 30,
